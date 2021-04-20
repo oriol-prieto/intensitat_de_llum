ff0befe7ff5d97fa92de40ca64df729c79f39465
oriol-prieto <82876000+oriol-prieto@users.noreply.github.com>
2021-04-20 12:28:19 +0200

    It's very good. You can moderate the brightnees

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
 basic.showLeds(`
-    # # # # #
-    # # # # #
-    # # # # #
+    . . # . .
+    . . # . .
+    . . # . .
     # # # # #
     # # # # #
     `)
+let llum = 0
+led.setBrightness(llum)
+serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        llum += 1
+        led.setBrightness(llum)
+        serial.writeLine("" + (llum))
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B)) {
+        llum += -1
+        led.setBrightness(llum)
+        serial.writeLine("" + (llum))
+    }
 })
